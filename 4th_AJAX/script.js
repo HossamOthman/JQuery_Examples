a63d4caab9ee83adb7c7132ce149171677b87492
Hossam Othman <rowheadrex@gmail.com>
2023-10-18 23:03:28 +0200

    get method

--- 4th_AJAX/script.js
+++ 4th_AJAX/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
 
+    $target = $('#target')
+
     //////////// Loading the HTML Document
     $classic_load = $('#classic-load');
     
     $classic_load.on('click', () => {
-        $('#target').load('test.html', function(resTxt, statTxt, xhr){
+        $target.load('test.html', function(resTxt, statTxt, xhr){
             if (statTxt == 'success') {
                 alert('it went fine!')
             } else if (statTxt == 'error'){
@@ -14,7 +16,12 @@ $(document).ready(function() {
     });
 
     //////////// Loading the HTML Document
-    
+    $classic_get = $('#classic-get');
 
-    
+    $classic_get.on('click', () => {
+        $.get('test.html', (data) => {
+            $target.html(data);
+        })
+    });
+
 });

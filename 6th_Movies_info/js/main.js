8a218c605f17532a371970366919dc8738b02aa4
Hossam Othman <rowheadrex@gmail.com>
2023-10-21 00:35:52 +0200

    output in index.html

--- 6th_Movies_info/js/main.js
+++ 6th_Movies_info/js/main.js
@@ -17,7 +17,18 @@ async function getMovies(searchText) {
         let movies = data.Search
         
         let output = '';
- 
+        $.each(movies, (index, movie) => {
+            output += `
+            <div class="col-md-3">
+                <div class="well text-center">
+                    <img src="${movie.Poster}">
+                    <h5>${movie.Title}</h5>
+                    <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+            </div>
+            `;
+        });
+        $('#movies').html(output);
 
     } catch (error) {
             console.log(error)

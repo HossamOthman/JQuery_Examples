5a9a8eaf1c85f8bc1583414f228b065f45917dae
Hossam Othman <rowheadrex@gmail.com>
2023-10-20 23:49:38 +0200

    async await fetch

--- 6th_Movies_info/js/main.js
+++ 6th_Movies_info/js/main.js
@@ -1,3 +1,30 @@
-require('dotenv').config;
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+        e.preventDefault();
 
-const moviesAPI = process.env.APIKEY;
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+
+    })
+});
+
+
+async function getMovies(searchText) {
+    try {
+        const response = await fetch(`https://www.omdbapi.com/?s=${searchText}&apikey=2aa54ec1`);
+        
+        const data = await response.json();
+        let movies = data.Search
+        
+        let output = '';
+ 
+
+    } catch (error) {
+            console.log(error)
+     
+    }
+    
+    
+     
+    
+}

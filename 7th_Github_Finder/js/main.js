8086e266240776a9f01de6a5fc031905556b03fa
Hossam Othman <rowheadrex@gmail.com>
2023-10-22 18:34:45 +0200

    done

--- 7th_Github_Finder/js/main.js
+++ 7th_Github_Finder/js/main.js
@@ -7,7 +7,31 @@ $(document).ready(() => {
         $.ajax({
             url: 'https://api.github.com/users/' + username
         }).done((user) => {
-            console.log(user);
+            $.ajax({
+                url: 'https://api.github.com/users/' + username + '/repos'
+            }).done((repos) => {
+                console.log(repos)
+                $.each(repos, (index, repo) => {
+                    $('#repos').append(`
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <strong>${repo.name}</strong>: ${repo.description}
+                            </div>
+                            <div class="col-md-3">
+                                <span class="badge badge-dark">Forks: ${repo.forks_count}</span>
+                                <span class="badge badge-primary">Watchers: ${repo.watchers_count}</span>
+                                <span class="badge badge-success">Stars: ${repo.stargazers_count}</span>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="${repo.html_url}" target="_blank" class="btn btn-dark">Repo Page</a>
+                            </div>
+                        </div>
+                    </div>
+                    `)
+                })
+            });
+
             $('#profile').html(`
             <div class="card border-primary mb-3" style="max-width: 100rem;">
                 <div class="card-header"><h3>${user.name}</h3></div>
@@ -35,7 +59,7 @@ $(document).ready(() => {
                 </div>
             <h3 class="page-header">Latest Repos</h3>
             <div id="repos"></div>
-            `)
+            `);
         });
 
 

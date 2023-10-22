e19fed8f304bf02cef4b3d6a18b14c0a302d5266
Hossam Othman <rowheadrex@gmail.com>
2023-10-22 18:17:33 +0200

    user section

--- 7th_Github_Finder/js/main.js
+++ 7th_Github_Finder/js/main.js
@@ -7,7 +7,35 @@ $(document).ready(() => {
         $.ajax({
             url: 'https://api.github.com/users/' + username
         }).done((user) => {
-            
+            console.log(user);
+            $('#profile').html(`
+            <div class="card border-primary mb-3" style="max-width: 100rem;">
+                <div class="card-header"><h3>${user.name}</h3></div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <img class="img-thumbnail avatar" src="${user.avatar_url}">
+                                <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
+                            </div>
+                            <div class="col-md-9">
+                                <span class="badge badge-dark">Public Repos: ${user.public_repos}</span>
+                                <span class="badge badge-primary">Public Gists: ${user.public_gists}</span>
+                                <span class="badge badge-success">Followers: ${user.followers}</span>
+                                <span class="badge badge-info">Following: ${user.following}</span>
+                                <br><br>
+                                <ul class="list-group">
+                                    <li class="list-group-item">Company: ${user.company}</li>
+                                    <li class="list-group-item">Website/blog: <a href="${user.blog}" target="_blank">${user.blog}</a></li>
+                                    <li class="list-group-item">Location: ${user.location}</li>
+                                    <li class="list-group-item">Member Since: ${user.created_at}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <h3 class="page-header">Latest Repos</h3>
+            <div id="repos"></div>
+            `)
         });
 
 
